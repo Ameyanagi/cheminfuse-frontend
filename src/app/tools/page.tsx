33336f8e3dff820c0a89ed1a1f8dbea49ee6f1de
Ameyanagi <77273474+Ameyanagi@users.noreply.github.com>
2023-03-13 15:52:15 -0400

    update tool

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,15 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const ToolsPage = () => {
 
     return (
         <div className="w-full text-center">
-            <div className='content-center w-full h-[40vh] bg-gradient-to-r from-[#548DFF] to-[#548DFF99] grid md:grid-cols-2'>
+            {/* <div className='content-center w-full h-[40vh] bg-gradient-to-r from-[#548DFF] to-[#548DFF99] grid md:grid-cols-2'>
                 <div className='items-center justify-center'>
                     <div className='flex flex-col text-white justify-center text-center items-center h-full mx-auto'>
                         Tools ページは現在作成中
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <div className="w-full text-left">
+                <div className="w-full p-10">
+                    <div className="max-w-[1240px] mx-auto">
+                        <p className="text-xl tracking widget uppercase text-[#5651e5] py-4">
+                            Tools
+                        </p>
 
+                        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                            <div className="relative flex items-center justify-center h-[300px] w-[300px] shadow-xl shadow-gray-400 rounded-xl group bg-gradient-to-r from-[#548DFF] to-[#548DFF99] mx-auto">
+                                <Link href="/tools/structure_optimization">
+                                    <Image
+                                        src="/images/Chemical-industry.png"
+                                        width={300}
+                                        height={300}
+                                        className="rounded-xl opacity-20 group-hover:opacity-5 object-fill"
+                                        alt="Structure Optimization"
+                                    />
+
+                                    <div className="absolute w-[80%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                                        <h3 className='text-2xl text-white traking-wider text-center py-5'>Structure Optimization using NN potential</h3>
+                                        <p className='pb-2 pt-2 text-white text-center'>ニューラルネットワークポテンシャルを利用した構造最適化 Webアプリ</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/components/Moleditor.tsx
+++ src/components/Moleditor.tsx
@@ -29,29 +29,43 @@ function Editor() {
           <p className="text-xl tracking widget uppercase text-[#5651e5] py-4">
             Structure Optimization using NN potential
           </p>
-          
+
           <div className="pb-10 text-xl">
-            How to use:
+            How to use: <br />
+            1. Draw your molecule in the editor or enter SMILES in the textbox. <br />
+            2. Press "Submit Structure". <br />
+            3. Wait for the results to be generated. This might take few minutes. <br />
+            4. csv file containing the optimized structures will be downloaded. <br />
           </div>
           <div className='p-4'>
-          <Jsme height="50vh" width="100%" options="oldlook,star" onChange={handleSmiles} />
+            <Jsme height="50vh" width="100%" options="oldlook,star" onChange={handleSmiles} />
             <form action="https://cheminfuse.com/torchaniapi/gen_conf_csv" method='POST'>
               <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>SMILES</label>
-                    <input
-                      className='border-2 rounded-lg p-3 flex border-gray-300'
-                      type='text'
-                      name='smiles'
-                      value={smiles}
-                      onChange={handleSmilesInput}
-                    />
-                  </div>
-                  <button className='w-full p-4 text-gray-100 mt-4'>
-                  Submit Structure
-                </button>
+                <label className='uppercase text-sm py-2'>SMILES</label>
+                <input
+                  className='border-2 rounded-lg p-3 flex border-gray-300'
+                  type='text'
+                  name='smiles'
+                  value={smiles}
+                  onChange={handleSmilesInput}
+                />
+              </div>
+              <button className='w-full p-4 text-gray-100 mt-4'>
+                Submit Structure
+              </button>
             </form>
           </div>
-
+          <div className="pb-10 text-xl">
+            Disclaimer: <br />
+            This tool is currently under development. <br />
+            Current setting will perform generation of 4000 random conformers, mmff94 energy minimization, clustering (10 groups at maximum), and then energy minimization using torchani ANI-1ccx, or ANI-2x depending on the elements. <br />
+            Elements supported for ANI-1ccx: H, C, N, O. <br />
+            Elements supported for ANI-2x: H, C, N, O, S, F, Cl. <br />
+            Charges are not supported.
+          </div>
+          <div className="pb-10 text-xl">
+            Blog post explaining the tool: I am working on it.
+          </div>
         </div>
       </div>
     </div>
